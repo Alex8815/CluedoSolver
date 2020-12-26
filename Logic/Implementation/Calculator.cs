96925278b7c67a68c545dff09a07bccec90e6453
alex <a.davies95@gmail.com>
2020-12-26 17:15:34 +0000

    initial commit

---
+++ Logic/Implementation/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Implementation
+{
+    public class Calculator : ICalculator
+    {
+
+    }
+}
